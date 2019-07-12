ae6b2b368e731dad13a609122ba2e4979ccbb8a7
nethergrim <c2q9450@gmail.com>
2019-07-12 17:10:41 +0300

    Jira

--- lib/fastlane/plugin/jira_set_fix_version/actions/jira_set_fix_version_action.rb
+++ lib/fastlane/plugin/jira_set_fix_version/actions/jira_set_fix_version_action.rb
@@ -13,7 +13,7 @@ module Fastlane
         require "jira-ruby"
 
         site         = params[:url]
-        context_path = "/secure"
+        context_path = ""
         auth_type    = :basic
         username     = params[:username]
         password     = params[:password]
